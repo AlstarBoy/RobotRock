f083209a915e9ebcbfcc0957ae539e565d1bdaa1
Alex Fosnseca <alexfonseca777@gmail.com>
2024-10-31 13:44:25 +0000

    New twinstick controls

---
+++ Assets/TwinStickMovement.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(CharacterController))]
+[RequireComponent(typeof(PlayerInput))]
+public class TwinStickMovement : MonoBehaviour
+{
+    [SerializeField] private float playerSpeed = 5f;
+    [SerializeField] private float gravityValue = -9.81f;
+    [SerializeField] private float controllerDeadzone = 0.1f;
+    [SerializeField] private float gamepadRotateSmoothing = 1000f;
+
+    [SerializeField] private bool isGamepad;
+
+    private CharacterController controller;
+
+    private Vector2 movement;
+    private Vector2 aim;
+
+    private Vector3 playerVelocity;
+
+    private PlayerControls playerControls;
+    private PlayerInput playerInput;
+
+    private void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+        playerControls = new PlayerControls();
+        playerInput = GetComponent<PlayerInput>();
+    }
+
+    private void OnEnable()
+    {
+        playerControls.Enable();
+    }
+    private void OnDisable()
+    {
+        playerControls.Disable();
+    }
+    void Update()
+    {
+        HandleInput();
+        HandleMovement();
+        HandleRotation();
+    }
+
+    void HandleInput()
+    {
+            movement = playerControls.Controls.Move.ReadValue<Vector2>(); 
+            aim = playerControls.Controls.Look.ReadValue<Vector2>();
+    }
+    void HandleMovement()
+    {
+        Vector3 move = new Vector3(movement.x, 8, movement.y);
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+    }
+    void HandleRotation()
+    {
+        if (isGamepad)
+        {
+            //Rotate our player
+        }
+    }
+
+    public void onDeviceChange(PlayerInput pi)
+    {
+        isGamepad = pi.currentControlScheme.Equals("Gamepad") ? true : false;
+
+    }
+}
