4dad200e0933ab35daefddf1f9b957cc341f2bfe
Alex Fonseca <alexfonseca777@gmail.com>
2024-12-12 14:57:21 +0000

    starting animation randomiser

---
+++ Assets/AnimationRandomiser.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class AnimationRandomiser : MonoBehaviour
+{
+    public int comboNumber;
+    public int randomNumber;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
