ea0724e7fc70e323db6d09efcfe0115080b90489
AlstarBoy <101668273+AlstarBoy@users.noreply.github.com>
2024-12-12 18:56:46 +0000

    animation randomiser function written

--- Assets/AnimationRandomiser.cs
+++ Assets/AnimationRandomiser.cs
@@ -1,9 +1,44 @@
+using System;
 using UnityEngine;
 
 public class AnimationRandomiser : MonoBehaviour
 {
     public int comboNumber;
     public int randomNumber;
+
+    void RandomiseAnimation(int comboNumber, float distance, int randNum)
+    {
+        if (distance < 3)
+        {   
+            if (comboNumber == 1)
+            {
+                randNum = UnityEngine.Random.Range(0, 5);
+            }
+            else if (comboNumber == 2)
+            {
+                randNum = UnityEngine.Random.Range(0, 6);
+            }
+            else if (comboNumber == 3)
+            {
+                randNum = UnityEngine.Random.Range(0, 4);
+            }
+        }
+        else if (distance > 3)
+        {
+            if (comboNumber == 1)
+            {
+                randNum = UnityEngine.Random.Range(0, 5);
+            }
+            else if (comboNumber == 2)
+            {
+                randNum = UnityEngine.Random.Range(0, 5);
+            }
+            else if (comboNumber == 3)
+            {
+                randNum = UnityEngine.Random.Range(0, 5);
+            }
+        }
+    }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -14,6 +49,6 @@ public class AnimationRandomiser : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        comboNumber = UnityEngine.Random.Range(0, 3);
     }
 }
